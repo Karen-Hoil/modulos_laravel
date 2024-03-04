07aff3fc1a8914944bd548054a92f89ec30eb1a9
Karen-Hoil <karenhoil2004@gmail.com>
2024-03-04 10:31:56 -0500

    vistas

---
+++ modulos_equipo/resources/views/dashPresi.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modulo6</title>
+    <!-- Agrega la CDN de Tailwind CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+    <!-- HeaderPresi component -->
+    <header>
+        <!-- Inserta el contenido del componente HeaderPresi aquí -->
+    </header>
+    
+    <div class="text-zinc-400 border-b border-zinc-400 px-5 py-3 text-xl mx-5">
+        Bienvenido (Presidente)!
+    </div>
+    
+    <div class="ag-format-container">
+        <div class="flex flex-wrap justify-between items-start px-0 md:px-4 py-8">
+            <div class="w-full md:w-1/2 mb-6 md:mb-0">
+                <a href="#" class="block p-4 rounded-xl ag-courses-item shadow hover:bg-blue-300 hover:text-black transition duration-300 relative mr-30 ml-20">
+                    <div class="h-32 mb-4 flex items-center justify-center">
+                        <img src="../img/proyectos.png" alt="Proyectos" class="h-full">
+                    </div>
+                    <div class="text-xl font-bold">Proyectos</div>
+                    <div>Proyectos del área de ingeniería</div>
+                </a>
+            </div>
+            <div class="w-full md:w-1/2 mb-6 md:mb-0">
+                <a href="#" class="block p-4 rounded-xl ag-courses-item shadow hover:bg-blue-300 hover:text-black transition duration-300 relative ml-10">
+                    <div class="h-32 mb-4 flex items-center justify-center">
+                        <img src="../img/asesores.png" alt="Documentos" class="h-full">
+                    </div>
+                    <div class="text-xl font-bold">Asesores</div>
+                    <div>Asesores del área de ingeniería</div>
+                </a>
+            </div>
+            <div class="w-full md:w-1/2 mb-6 md:mb-0">
+                <a href="#" class="block p-4 rounded-xl ag-courses-item shadow hover:bg-blue-300 hover:text-black transition duration-300 relative mr-30 ml-10">
+                    <div class="h-32 mb-4 flex items-center justify-center">
+                        <img src="../img/estudiantes.png" alt="Asesores" class="h-full">
+                    </div>
+                    <div class="text-xl font-bold">Estudiantes</div>
+                    <div>Estudiantes del área de ingeniería</div>
+                </a>
+            </div>
+            <div class="w-full md:w-1/2 mb-6 md:mb-0">
+                <a href="#" class="block p-4 rounded-xl ag-courses-item shadow hover:bg-blue-300 hover:text-black transition duration-300 relative ml-10">
+                    <div class="h-32 mb-4 flex items-center justify-center">
+                        <img src="../img/documentos.png" alt="Estudiantes" class="h-full">
+                    </div>
+                    <div class="text-xl font-bold">Documentos</div>
+                    <div>Documentos de estudiantes y asesores</div>
+                </a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ modulos_equipo/resources/views/modulo4.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Modulo 4</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="text-zinc-400 border-b border-zinc-400 px-5 py-3 text-xl mx-5">
+        Bienvenido (docente)!
+      </div>
+      <div class="titulo">
+        <h1 class="px-10 py-3 text-3xl font-bold">Control de alumnos asesorados</h1>
+      </div>
+      <div class="container mt-10">
+        <div class="flex flex-wrap justify-center">
+          <div class="container__items border rounded-lg shadow-md p-4 mx-10 my-5 max-w-md">
+            <h6 class="text-xl text-center text-blue-600">Estudiante #</h6>
+            <p>Proyecto:$proyecto</p>
+            <p>Matricula:$matricula</p>
+          </div>
+          <div class="container__items border rounded-lg shadow-md p-4 mx-10 my-5 max-w-md">
+            <h6 class="text-xl text-center text-blue-600">Estudiante #</h6>
+            <p>Proyecto:$proyecto</p>
+            <p>Matricula:$matricula</p>
+          </div>
+        </div>
+        <div class="flex flex-wrap justify-center">
+          <div class="container__items border rounded-lg shadow-md p-4 mx-10 my-5 max-w-md">
+            <h6 class="text-xl text-center text-blue-600">Estudiante #</h6>
+            <p>Proyecto:$proyecto</p>
+            <p>Matricula:$matricula</p>
+          </div>
+          <div class="container__items border rounded-lg shadow-md p-4 mx-10 my-5 max-w-md">
+            <h6 class="text-xl text-center text-blue-600">Estudiante #</h6>
+            <p>Proyecto:$proyecto</p>
+            <p>Matricula:$matricula</p>
+          </div>
+        </div>
+        <div class="flex flex-wrap justify-center">
+          <div class="container__items border rounded-lg shadow-md p-4 mx-10 my-5 max-w-md">
+            <h6 class="text-xl text-center text-blue-600">Estudiante #</h6>
+            <p>Proyecto:$proyecto</p>
+            <p>Matricula:$matricula</p>
+          </div>
+          <div class="container__items border rounded-lg shadow-md p-4 mx-10 my-5 max-w-md">
+            <h6 class="text-xl text-center text-blue-600">Estudiante #</h6>
+            <p>Proyecto:$proyecto</p>
+            <p>Matricula:$matricula</p>
+          </div>
+        </div>
+        <div class="flex flex-wrap justify-center">
+          <div class="container__items border rounded-lg shadow-md p-4 mx-10 my-5 max-w-md">
+            <h6 class="text-xl text-center text-blue-600">Estudiante #</h6>
+            <p>Proyecto:$proyecto</p>
+            <p>Matricula:$matricula</p>
+          </div>
+          <div class="container__items border rounded-lg shadow-md p-4 mx-10 my-5 max-w-md">
+            <h6 class="text-xl text-center text-blue-600">Estudiante #</h6>
+            <p>Proyecto:$proyecto</p>
+            <p>Matricula:$matricula</p>
+          </div>
+        </div>
+      </div>      
+</body>
+</html>
