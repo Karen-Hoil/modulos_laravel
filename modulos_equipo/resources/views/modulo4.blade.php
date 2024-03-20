e0afa2b4a2649400445968a3bed2c2fb0bc7c491
MaxQuin1 <maximoquinteroescobar8@gmail.com>
2024-03-19 20:04:05 -0600

    solo falta que genere los archivos

--- modulos_equipo/resources/views/modulo4.blade.php
+++ modulos_equipo/resources/views/modulo4.blade.php
@@ -11,121 +11,33 @@
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body>
-  @foreach ($estudiantes as $estudiante)
   @section ('contenido1')
     <div class="text-zinc-400 border-b border-zinc-400 px-5 py-3 text-xl mx-5">
-        Bienvenido (docente)!
+      Bienvenido (docente)!
     </div>
-      <div class="titulo">
-        <h1 class="px-10 py-3 text-3xl font-bold">Control de alumnos asesorados</h1>
+    <div class="titulo">
+      <h1 class="px-10 py-3 text-3xl font-bold">Control de alumnos asesorados</h1>
+    </div>
+    <div class="flex flex-wrap my-10 justify-center">
+      @foreach ($estudiantes as $estudiante)
+      <div class="flex flex-col border rounded-lg shadow-lg px-8 m-5 bg-white w-1/4 max-w-1/4">
+        <h6 class="text-xl text-center text-blue-600 m-3">{{$estudiante->nombre}}</h6>
+        <div class="flex">
+          <img src="{{$estudiante->foto}}" alt="profile-picture" class="relative h-20 rounded-xl shadow-lg" />
+          <div class="pl-5">
+            <p class="">{{$estudiante->proyecto}}</p>
+            <p class="">{{$estudiante->matricula}}</p>
+            <p class="">{{$estudiante->correo}}</p>
+          </div>
+        </div>
+        <div class="flex justify-center m-5 gap-2">
+          <button class="bg-blue-600 p-2 rounded-lg text-white">Generar</button>
+          <button class="bg-blue-600 p-2 rounded-lg text-white">Generar</button>
+          <button class="bg-blue-600 p-2 rounded-lg text-white">Generar</button>
+        </div>
       </div>
-      <div class="flex flex-wrap mt-10 justify-center">
-          <div class="flex flex-col border rounded-lg shadow-lg px-7 m-5 bg-white">
-            <h6 class="text-xl text-center text-blue-600 m-3">{{$estudiante->nombre}}</h6>
-            <div class="flex">
-              <img src="{{$estudiante->foto}}" alt="profile-picture" class="relative m-4 h-20 rounded-xl shadow-lg" />
-              <div class="m-5 mr-10 mb-10">
-                <p class="">{{$estudiante->proyecto}}</p>
-                <p class="">{{$estudiante->matricula}}</p>
-                <p class="">{{$estudiante->correo}}</p>
-              </div>
-            </div>
-          </div>
-          <div class="flex flex-col border rounded-lg shadow-lg px-7 m-5 bg-white">
-            <h6 class="text-xl text-center text-blue-600 m-3">{{$estudiante->nombre}}</h6>
-            <div class="flex">
-              <img src="{{$estudiante->foto}}" alt="profile-picture" class="relative m-4 h-20 rounded-xl shadow-lg" />
-              <div class="m-5 mr-10 mb-10">
-                <p class="">{{$estudiante->proyecto}}</p>
-                <p class="">{{$estudiante->matricula}}</p>
-                <p class="">{{$estudiante->correo}}</p>
-              </div>
-            </div>
-          </div>
-          <div class="flex flex-col border rounded-lg shadow-lg px-7 m-5 bg-white">
-            <h6 class="text-xl text-center text-blue-600 m-3">{{$estudiante->nombre}}</h6>
-            <div class="flex">
-              <img src="{{$estudiante->foto}}" alt="profile-picture" class="relative m-4 h-20 rounded-xl shadow-lg" />
-              <div class="m-5 mr-10 mb-10">
-                <p class="">{{$estudiante->proyecto}}</p>
-                <p class="">{{$estudiante->matricula}}</p>
-                <p class="">{{$estudiante->correo}}</p>
-              </div>
-            </div>
-          </div>
-        
-        
-          <div class="flex flex-col border rounded-lg shadow-lg px-7 m-5 bg-white">
-            <h6 class="text-xl text-center text-blue-600 m-3">{{$estudiante->nombre}}</h6>
-            <div class="flex">
-              <img src="{{$estudiante->foto}}" alt="profile-picture" class="relative m-4 h-20 rounded-xl shadow-lg" />
-              <div class="m-5 mr-10 mb-10">
-                <p class="">{{$estudiante->proyecto}}</p>
-                <p class="">{{$estudiante->matricula}}</p>
-                <p class="">{{$estudiante->correo}}</p>
-              </div>
-            </div>
-          </div>
-          <div class="flex flex-col border rounded-lg shadow-lg px-7 m-5 bg-white">
-            <h6 class="text-xl text-center text-blue-600 m-3">{{$estudiante->nombre}}</h6>
-            <div class="flex">
-              <img src="{{$estudiante->foto}}" alt="profile-picture" class="relative m-4 h-20 rounded-xl shadow-lg" />
-              <div class="m-5 mr-10 mb-10">
-                <p class="">{{$estudiante->proyecto}}</p>
-                <p class="">{{$estudiante->matricula}}</p>
-                <p class="">{{$estudiante->correo}}</p>
-              </div>
-            </div>
-          </div>
-          <div class="flex flex-col border rounded-lg shadow-lg px-7 m-5 bg-white">
-            <h6 class="text-xl text-center text-blue-600 m-3">{{$estudiante->nombre}}</h6>
-            <div class="flex">
-              <img src="{{$estudiante->foto}}" alt="profile-picture" class="relative m-4 h-20 rounded-xl shadow-lg" />
-              <div class="m-5 mr-10 mb-10">
-                <p class="">{{$estudiante->proyecto}}</p>
-                <p class="">{{$estudiante->matricula}}</p>
-                <p class="">{{$estudiante->correo}}</p>
-              </div>
-            </div>
-          </div>
-        
-        
-          <div class="flex flex-col border rounded-lg shadow-lg px-7 m-5 bg-white">
-            <h6 class="text-xl text-center text-blue-600 m-3">{{$estudiante->nombre}}</h6>
-            <div class="flex">
-              <img src="{{$estudiante->foto}}" alt="profile-picture" class="relative m-4 h-20 rounded-xl shadow-lg" />
-              <div class="m-5 mr-10 mb-10">
-                <p class="">{{$estudiante->proyecto}}</p>
-                <p class="">{{$estudiante->matricula}}</p>
-                <p class="">{{$estudiante->correo}}</p>
-              </div>
-            </div>
-          </div>
-          <div class="flex flex-col border rounded-lg shadow-lg px-7 m-5 bg-white">
-            <h6 class="text-xl text-center text-blue-600 m-3">{{$estudiante->nombre}}</h6>
-            <div class="flex">
-              <img src="{{$estudiante->foto}}" alt="profile-picture" class="relative m-4 h-20 rounded-xl shadow-lg" />
-              <div class="m-5 mr-10 mb-10">
-                <p class="">{{$estudiante->proyecto}}</p>
-                <p class="">{{$estudiante->matricula}}</p>
-                <p class="">{{$estudiante->correo}}</p>
-              </div>
-            </div>
-          </div>
-          <div class="flex flex-col border rounded-lg shadow-lg px-7 m-5 bg-white">
-            <h6 class="text-xl text-center text-blue-600 m-3">{{$estudiante->nombre}}</h6>
-            <div class="flex">
-              <img src="{{$estudiante->foto}}" alt="profile-picture" class="relative m-4 h-20 rounded-xl shadow-lg" />
-              <div class="m-5 mr-10 mb-10">
-                <p class="">{{$estudiante->proyecto}}</p>
-                <p class="">{{$estudiante->matricula}}</p>
-                <p class="">{{$estudiante->correo}}</p>
-              </div>
-            </div>
-          </div>
-        
-      </div>  
-      @endSection
-      @endforeach    
+      @endforeach 
+      </div> 
+      @endSection   
 </body>
 </html>
